1d17311b512ff329b2b3b9e1a486ac6c5aaef6f2
Kishor Balgi <kishorbalgi@gmail.com>
2023-08-23 21:02:36 +0530

    feat: Added Business owner page

---
+++ app/register/business-owner/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegisterBusiness from "../../../components/Authentication/registerBusiness";
+
+const signUp = () => {
+  return (
+    <React.Fragment>
+      <RegisterBusiness />
+    </React.Fragment>
+  );
+};
+
+export default signUp;
